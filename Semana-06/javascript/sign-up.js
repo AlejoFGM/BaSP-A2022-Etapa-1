98e2ffc0f4c7d78ed3780a8fb837789395eb7d3c
AlejoFGM <alejo.fgm@gmail.com>
2022-09-26 17:50:55 -0300

    BaSP-W06-2022: date, phone and address validation in sign-up page

--- Semana-06/javascript/sign-up.js
+++ Semana-06/javascript/sign-up.js
@@ -9,18 +9,18 @@ window.onload = function() {
     }
 
     name.onfocus = function() {
-        name.classList.remove("succes")
-        name.classList.remove("error")
+        name.classList.remove("succes");
+        name.classList.remove("error");
     }
 
     name.onblur = function() {
         if(!validateName()) {
-            name.classList.remove("succes")
-            name.classList.add("error")
+            name.classList.remove("succes");
+            name.classList.add("error");
         }
         if(validateName()) {
-            name.classList.remove("error")
-            name.classList.add("succes")
+            name.classList.remove("error");
+            name.classList.add("succes");
         }
     }
 
@@ -34,18 +34,18 @@ window.onload = function() {
     }
 
     lastName.onfocus = function() {
-        lastName.classList.remove("succes")
-        lastName.classList.remove("error")
+        lastName.classList.remove("succes");
+        lastName.classList.remove("error");
     }
 
     lastName.onblur = function() {
         if(!validateLastName()) {
-            lastName.classList.remove("succes")
-            lastName.classList.add("error")
+            lastName.classList.remove("succes");
+            lastName.classList.add("error");
         }
         if(validateLastName()) {
-            lastName.classList.remove("error")
-            lastName.classList.add("succes")
+            lastName.classList.remove("error");
+            lastName.classList.add("succes");
         }
     }
 
@@ -59,18 +59,103 @@ window.onload = function() {
     }
 
     dni.onfocus = function() {
-        dni.classList.remove("succes")
-        dni.classList.remove("error")
+        dni.classList.remove("succes");
+        dni.classList.remove("error");
     }
 
     dni.onblur = function() {
         if(!validateDni()) {
-            dni.classList.remove("succes")
-            dni.classList.add("error")
+            dni.classList.remove("succes");
+            dni.classList.add("error");
         }
         if(validateDni()) {
-            dni.classList.remove("error")
-            dni.classList.add("succes")
+            dni.classList.remove("error");
+            dni.classList.add("succes");
+        }
+    }
+
+    var date = document.getElementById("date");
+
+    function calculateAge(date) {
+        var today = new Date();
+        var date2 = new Date(date)
+        var age = today.getFullYear() - date2.getFullYear();
+        var months = today.getMonth() - date2.getMonth();
+        if (months < 0 || (months === 0 && today.getDate() < date2.getDate())) {
+            age--;
+        }
+        return age;
+    }
+
+    function validateDate() {
+        if(calculateAge(date.value) < 18) {
+            return false
+        } return true
+    }
+
+    date.onfocus = function() {
+        date.classList.remove("succes");
+        date.classList.remove("error");
+    }
+
+    date.onblur = function() {
+        if(validateDate()) {
+            date.classList.remove("error");
+            date.classList.add("succes");
+        }
+        if(!validateDate()) {
+            date.classList.remove("succes");
+            date.classList.add("error");
+        }
+    }
+
+    var phone = document.getElementById("phone");
+    var phoneExp = /^[0-9]{10,10}$/;
+
+    function validatePhone() {
+        if(phoneExp.test(phone.value)) {
+            return true;
+        } else return false;
+    }
+
+    phone.onfocus = function() {
+        phone.classList.remove("succes");
+        phone.classList.remove("error");
+    }
+
+    phone.onblur = function() {
+        if(!validatePhone()) {
+            phone.classList.remove("succes");
+            phone.classList.add("error");
+        }
+        if(validatePhone()) {
+            phone.classList.remove("error");
+            phone.classList.add("succes");
+        }
+    }
+
+    var address = document.getElementById("address");
+    var addressExp = /^[a-zA-ZñÑáéíóúÁÉÍÓÚ]+[\s]+[0-9]+$/;
+
+    function validateAddress() {
+        if(addressExp.test(address.value)) {
+            return true;
+        } else return false;
+    }
+
+    address.onfocus = function() {
+        address.classList.remove("succes");
+        address.classList.remove("error");
+    }
+
+    address.onblur = function() {
+        if(!validateAddress()) {
+            address.classList.remove("succes");
+            address.classList.add("error");
+        }
+        if(validateAddress()) {
+            address.classList.remove("error");
+            address.classList.add("succes");
         }
     }
 }
